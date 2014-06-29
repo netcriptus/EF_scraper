2f25a6111d08dab8c316c57f0a0509761b0c64b8
Fernando Cezar <netcriptus@gmail.com>
2014-06-29 09:20:28 -0300

    Some basic (yet inexpressive) tests

---
+++ scraper/tests/test_facebook.py
@@ -0,0 +1,21 @@
+from scraper import db
+from scraper.tests import BaseTestCase
+
+
+class AuthorizationTest(BaseTestCase):
+
+    def test_asyncronous_response(self):
+        db.create_all()
+        response = self.client.get("/facebook/10202156179485176")
+        self.assertEqual(response.status_code, 202)
+        db.session.remove()
+        db.drop_all()
+
+    def test_parsed_profile_response(self):
+        db.create_all()
+        response = self.client.get("/facebook/10202156179485176")
+        self.assertEqual(response.status_code, 202)
+        response = self.client.get("/facebook/10202156179485176")
+        self.assertEqual(response.status_code, 200)
+        db.session.remove()
+        db.drop_all()
