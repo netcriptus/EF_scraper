1dfdec67970a83a4e02b8bb6ef89811da48559cf
Fernando Cezar <netcriptus@gmail.com>
2014-06-29 11:23:58 -0300

    CELERY_IMPORTS is apparently useless for flask

--- scraper/settings/base.py
+++ scraper/settings/base.py
@@ -3,6 +3,5 @@ CSRF_ENABLED = True
 SQLALCHEMY_DATABASE_URI = "mysql://scraper:scraper@127.0.0.1/scraper"
 BROKER_URL = 'redis://localhost:6379',
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_IMPORTS = ("tasks",)
 FACEBOOK_BASE_URL = 'https://graph.facebook.com/fql'
 TWITTER_BASE_URL = 'http://twitter.com/'
