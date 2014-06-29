2402c4a93deade0fde8f11b9bff40577995778ed
Fernando Cezar <netcriptus@gmail.com>
2014-06-29 09:21:13 -0300

    Redefine configurations

--- scraper/settings/base.py
+++ scraper/settings/base.py
@@ -1,8 +1,8 @@
 DEBUG = True
 CSRF_ENABLED = True
 SQLALCHEMY_DATABASE_URI = "mysql://scraper:scraper@127.0.0.1/scraper"
-CELERY_BROKER_URL = 'redis://localhost:6379',
+BROKER_URL = 'redis://localhost:6379',
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-FACEBOOK = {'consumer_key': '1503901056508224',
-            'consumer_secret': 'd255714adb8bd73ce7a0c56572ca4f98'}
+CELERY_IMPORTS = ("tasks",)
+FACEBOOK_BASE_URL = 'https://graph.facebook.com/fql'
 TWITTER_BASE_URL = 'http://twitter.com/'
